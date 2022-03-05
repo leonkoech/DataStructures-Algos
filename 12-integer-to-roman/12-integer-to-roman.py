1ff1abb13cf54e5aa3c5aca56f28d504ba5d23e6
leon kipkoech <39020723+leonkoech@users.noreply.github.com>
2022-03-05 02:35:21 -0500

    Time: 73 ms (50.40%), Space: 13.9 MB (91.39%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -1,6 +1,6 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        1165
+        
         answer=""
         myList=[(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), 
                   (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), 
