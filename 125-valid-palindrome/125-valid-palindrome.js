edc87b482a95f222276bf393a6bdc4253d1e1620
leon kipkoech <39020723+leonkoech@users.noreply.github.com>
2022-07-02 11:40:08 -0700

    Time: 154 ms (11.31%), Space: 44.2 MB (82.12%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -4,17 +4,20 @@
  */
 var isPalindrome = function(s) {
     if (s.length == 1){ return true }
+    
     s = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()
+    
     var p1 = 0;
     var p2 = s.length-1
+    
     while (p1<=p2){
          if (s[p1] != s[p2]){
-             console.log([s[p1],s[p2]])
              return false
          }
         p1++
         p2--
     }
+    
     return true
        
 };
